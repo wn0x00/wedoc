dc1c8f0ee0de82789e18e7eb61dc03aaa07e9d89
jx <320753691@qq.com>
2023-01-16 19:31:26 +0800

    feat: 增加代码提示

--- wedoc/api/document.py
+++ wedoc/api/document.py
@@ -2,8 +2,29 @@ from wedoc.api.base import WedocApiBase
 
 
 class Document(WedocApiBase):
-    def create_doc(self, doc_name, doc_type, spaceid="", fatherid="", admin_users=[]):
-        """创建文档"""
+    def create_doc(
+        self,
+        doc_name: str,
+        doc_type: int,
+        spaceid: str = "",
+        fatherid: str = "",
+        admin_users: list = [],
+    ) -> dict:
+        """
+        创建文档
+        :param doc_name:
+        :param doc_type:
+        :param spaceid:
+        :param fatherid:
+        :param admin_users:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.doc.create_doc("doc_name", "doc_type")
+        """
         api = "/wedoc/create_doc"
         pyload = {
             "doc_type": doc_type,
@@ -13,22 +34,57 @@ class Document(WedocApiBase):
         res = self.request("post", api, pyload=pyload)
         return res
 
-    def rename_doc(self, docid, new_name):
-        """重命名文档"""
+    def rename_doc(self, docid: str, new_name: str) -> dict:
+        """
+        重命名文档
+
+        :param doc_name:
+        :param docid:
+        :param new_name:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.doc.rename_doc("docid", "new_name")
+        """
         api = "/wedoc/rename_doc"
         pyload = {"docid": docid, "new_name": new_name}
         res = self.request("post", api, pyload=pyload)
         return res
 
-    def del_doc(self, docid):
-        """删除文档"""
+    def del_doc(self, docid: str) -> dict:
+        """
+        删除文档
+
+        :param docid:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.doc.del_doc("docid")
+        """
         api = "/wedoc/del_doc"
         pyload = {"docid": docid}
         res = self.request("post", api, pyload=pyload)
         return res
 
-    def get_doc_base_info(self, docid):
-        """获取文档基础信息"""
+    def get_doc_base_info(self, docid: str) -> dict:
+
+        """
+        获取文档基础信息
+        :param docid:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.doc.get_doc_base_info("docid")
+        """
         api = "/wedoc/get_doc_base_info"
         pyload = {"docid": docid}
         res = self.request("post", api, pyload=pyload)
--- wedoc/api/spreadsheet.py
+++ wedoc/api/spreadsheet.py
@@ -2,22 +2,57 @@ from wedoc.api.base import WedocApiBase
 
 
 class Spreadsheet(WedocApiBase):
-    def get_sheet_properties(self, docid):
-        """获取表格行列信息"""
+    def get_sheet_properties(self, docid: str) -> dict:
+        """
+        获取表格行列信息
+
+        :param docid:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.wb.get_sheet_properties("docid")
+        """
+
         api = "/wedoc/spreadsheet/get_sheet_properties"
         pyload = {"docid": docid}
         res = self.request("post", api, pyload=pyload)
         return res
 
     def get_sheet_range_data(self, docid, sheet_id, range):
-        """获取表格数据"""
+        """
+        获取表格行列信息
+        :param docid:
+        :param sheet_id:
+        :param range:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.wb.get_sheet_range_data("docid", "range")
+        """
         api = "/wedoc/spreadsheet/get_sheet_range_data"
         pyload = {"docid": docid, "sheet_id": sheet_id, "range": range}
         res = self.request("post", api, pyload=pyload)
         return res
 
     def batch_update(self, docid):
-        """编辑表格内容"""
+        """
+        编辑表格内容
+        :param docid:
+        :return:
+
+        使用示例
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.wb.batch_update("docid")
+        """
+
         api = "/wedoc/spreadsheet/get_sheet_range_data"
         pyload = {
             "docid": docid,
