ebe1a46e134582c92764c57991960e9aa4c0fd36
jx <320753691@qq.com>
2023-01-16 09:30:13 +0800

    feat: 完成基础功能

--- wedoc/__init__.py
+++ wedoc/__init__.py
@@ -4,8 +4,20 @@ from wedoc.client import WedocClientBase
 
 
 class WedocClient(WedocClientBase):
-    doc = Document()
-    wb = Spreadsheet()
+    doc: Document = Document()
+    wb: Spreadsheet = Spreadsheet()
 
     def __init__(self, corpid, corpsecret) -> None:
+        """
+        对企业微信自建应用初始化
+
+        :param corpid: 企业 ID 标识
+        :param corpsecret: 企业自建应用密钥
+
+        使用示例::
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.doc.create_doc("doc_name", "doc_type")
+        """
         super().__init__(corpid, corpsecret)
